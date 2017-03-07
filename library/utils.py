d2a039366eff372182f953e9e0a76efd49b3bdc8
Italo Santos <okdokk@gmail.com>
2017-03-06 21:21:28 -0300

    Adding general utilities functions

---
+++ library/utils.py
@@ -0,0 +1,44 @@
+# utils.py
+# 
+# General utilities functions
+
+class bcolors:
+	""" Print a terminal output colored text by using ANSI escape sequences """
+
+    OKGREEN = '\033[92m' # Green
+    WARNING = '\033[93m' # Yellow
+    FAIL = '\033[91m' # Red
+    OKBLUE = '\033[94m' # Blue
+    HEADER = '\033[95m' # Pink
+    BOLD = '\033[1m' # Bold text
+    UNDERLINE = '\033[4m' # Underline text
+    ENDC = '\033[0m' # EOL
+
+def confirm(prompt=None, resp=False):
+    """ Prompts yes or no response to user user.
+    Returns True for yes and False for no """
+    
+    # Set a default message if no param was received
+    if prompt is None:
+        prompt = 'Confirm?'
+
+    # Check if 'resp' was set to True the default answer will be set to Y (yes),
+    # if varible wasn't set the default answer will be N (No)
+    if resp:
+        prompt = '%s [%s/%s]: ' % (prompt, 'Y', 'n')
+    else:
+        prompt = '%s [%s/%s]: ' % (prompt, 'y', 'N')
+        
+    # Create a while loop to read the answer from terminal.
+    # If no answer, the print will be returned
+    while True:
+        answer = raw_input(prompt)
+        if not answer:
+            return resp
+        if answer not in ['y', 'Y', 'n', 'N']:
+            print 'Please enter y or n.\n'
+            continue
+        if answer == 'y' or answer == 'Y':
+            return True
+        if answer == 'n' or answer == 'N':
+            return False
